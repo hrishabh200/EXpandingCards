85d0fa74f67c4cdf872653527735fb69251cd25a
hrishabh200 <hrishabhppandey@gmail.com>
2022-03-16 14:09:40 +0530

    Added for loop

--- ExpandingCards.js
+++ ExpandingCards.js
@@ -1,29 +1,31 @@
 // ALL we have to do is change class from not active to active 
 const panels = document.querySelectorAll('.panel') 
 //querySelector to select css elements as a node or as an array 
+// panels.forEach(panel => {
+//     panel.addEventListener('click', () => {
+//         removeActiveClasses() // we remove active on other classes which are active
+//         panel.classList.add('active') //panel=panels[i]
+//     })
+// })
 
-panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses() // we remove active on other classes which are active
-        panel.classList.add('active')
-    })
-})
-
-function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
+// function removeActiveClasses() {
+//     panels.forEach(panel => {
+//         panel.classList.remove('active')
+//     })
+// }
+for(let i=0;i<panels.length;i++)
+{
+    panels[i].addEventListener('click',
+    function occur() {
+        removeActiveClasses();
+        panels[i].classList.add('active');
     })
 }
-// for(let i=0;i<panels.length;i++)
-// {
-//     panels[i].addEventListener('click',removeActiveClasses())
-//     panels[i].classList.add('active');
+function removeActiveClasses()
+{
+for(let i=0;i<panels.length;i++)
+{
+    panels[i].classList.remove('active');
+}
 
-// }
-// function removeActiveClasses()
-// {
-// for(let i=0;i<panels.length;i++)
-// {
-//     panels[i].classList.remove('active');
-// }
-// }
+}
